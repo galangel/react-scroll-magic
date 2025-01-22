fbd3edfb30837021d23fcb4fa807526c7d756568
Gal Angel <gal.angel@logz.io>
2025-01-22 07:57:56 +0200

    Publish to npm workflow (#31)

--- dist/Scroll/Scroll.stories.d.ts
+++
@@ -1,7 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react';
-import { Scroll } from './index';
-
-declare const _default: Meta;
-export default _default;
-type Story = StoryObj<typeof Scroll>;
-export declare const BasicExample: Story;
