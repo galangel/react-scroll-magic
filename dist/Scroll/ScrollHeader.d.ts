925047402ea0f9793a954d44ac07c127b727ceba
galangel <gal0angel@gmail.com>
2025-01-22 07:55:17 +0200

    Remove unused Scroll components and add npmignore for package publishing

--- dist/Scroll/ScrollHeader.d.ts
+++
@@ -1,6 +0,0 @@
-import { default as React } from 'react';
-
-interface IScrollHeaderProps extends React.PropsWithChildren<React.LiHTMLAttributes<HTMLLIElement>> {
-}
-export declare const ScrollHeader: React.FC<IScrollHeaderProps>;
-export {};
--- dist/Scroll/ScrollList.d.ts
+++
@@ -1,3 +0,0 @@
-import { default as React } from 'react';
-
-export declare const ScrollList: React.FC<React.PropsWithChildren<React.LiHTMLAttributes<HTMLUListElement>>>;
