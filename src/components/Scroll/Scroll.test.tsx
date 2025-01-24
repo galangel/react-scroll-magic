71ba03b22ccd610d83f8f1b64b0c109518601459
galangel <gal0angel@gmail.com>
2025-01-24 14:44:05 +0200

    Refactor Scroll component tests to simplify rendering and improve test coverage

--- src/components/Scroll/Scroll.test.tsx
+++ src/components/Scroll/Scroll.test.tsx
@@ -2,17 +2,10 @@ import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { Scroll } from '.';
 
-// describe('Scroll component', () => {
-//   it('renderes Scroll component', () => {
-//     render(
-//       <Scroll style={{ height: '100px' }}>
-//         <Scroll.Header style={{ height: '20px' }}>header1</Scroll.Header>
-//         <Scroll.Item style={{ height: '20px' }}>item1</Scroll.Item>
-//         <Scroll.Header style={{ height: '20px' }}>header2</Scroll.Header>
-//         <Scroll.Item style={{ height: '20px' }}>item2</Scroll.Item>
-//       </Scroll>,
-//     );
+describe('Scroll component', () => {
+  it('renderes Scroll component', () => {
+    render(<Scroll items={[{ render: () => <span>hello</span> }]} />);
 
-//     expect(screen.getByText('header1')).toBeTruthy();
-//   });
-// });
+    expect(screen.getByText('hello')).toBeTruthy();
+  });
+});
